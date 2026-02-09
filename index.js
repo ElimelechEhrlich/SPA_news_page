ba93456bc8d645c71baa5236442695106d89f3b3
Elimelech Ehrlich <ee525566@gmail.com>
2026-02-09 18:03:28 +0200

    create story page

--- index.js
+++ index.js
@@ -1,62 +1,94 @@
 import { news } from "./main.js"
 
 const root = document.getElementById('root')
-const header = document.getElementById('header')
+const header = document.getElementById('nav')
 const page_content = document.getElementById('content')
 const footer = document.getElementById('footer')
 
-
-function createNavHeader() {
-    createLink('News')
-    createLink('Home')
-    createLink('Create Story')
+function checkLocalStorage() {
+    if (localStorage.data) return JSON.parse(localStorage.getItem('data'))
+    else return news
 }
+
+
+
+function linkEvent(link) {
+    link.addEventListener('click', e => {
+        e.preventDefault()
+        if (link.textContent === 'Home') {
+            page_content.innerHTML = ''
+            contentLoading()
+        }
+        else if (link.textContent === 'News') {
+            page_content.innerHTML = ''
+            for (let i = 0; i < news.length; i++) {
+                createNewsContent(news[i].urlToImage, news[i].content, news[i].description)
+            }
+        }
+        if (link.textContent === 'CreateStory') {
+            page_content.innerHTML = ''
+            storyCreationPage()
+        }
+    })
+}
+
 
 function createLink(titleContent) {
     const title = document.createElement('a')
     title.textContent = titleContent
     title.classList.add('link')
-    header.appendChild(title)
+    title.classList.add(titleContent)
+    if (titleContent === 'News') header.appendChild(title)
+    else {
+        let headerDiv = document.getElementById('headerDiv')
+        if (!headerDiv) {
+            headerDiv = document.createElement('div')
+            headerDiv.id = 'headerDiv'
+            header.appendChild(headerDiv)
+        }
+        title.classList.add('headerDiv')
+        headerDiv.appendChild(title)
+    }
     linkEvent(title)
 }
 
-function linkEvent(link) {
-    link.addEventListener('click', e => {
-        e.preventDefault()
-        if (link.textContent === 'News') {
-            header.innerHTML = ''
-            createNavHeader()
-            const data = document.createElement('div')
-            data.textContent = data
-        }
-    })
+function createNavHeader() {
+    createLink('News')
+    createLink('Home')
+    createLink('CreateStory')
 }
-
 
 function createHomeContent(d, id) {
     const news_card = document.createElement('section')
     news_card.classList.add('news')
     news_card.id = id
     page_content.appendChild(news_card)
+    page_content.classList.remove('column')
+    page_content.classList.add('row')
+    page_content.classList.remove('news_content')
+    page_content.classList.add('home_content')
     const data = { urlToImage: d.urlToImage, author: d.author, title: d.title, content: d.content, description: d.description }
     const titlep = document.createElement('p')
     titlep.textContent = data.title
     titlep.classList.add('title')
     news_card.appendChild(titlep)
-    const image = document.createElement('img')
-    image.src = data.urlToImage
-    image.classList.add('image')
-    news_card.appendChild(image)
     const authorp = document.createElement('p')
     authorp.textContent = data.author
     authorp.classList.add('author')
     news_card.appendChild(authorp)
+    const image = document.createElement('img')
+    image.src = data.urlToImage
+    image.classList.add('image')
+    news_card.appendChild(image)
 }
 function createNewsContent(urlToImage, content, description) {
-    page_content.innerHTML = ''
     const news_card = document.createElement('section')
     news_card.classList.add('news')
     page_content.appendChild(news_card)
+    page_content.classList.remove('row')
+    page_content.classList.add('column')
+    page_content.classList.remove('home_content')
+    page_content.classList.add('news_content')
     const descriptionp = document.createElement('p')
     descriptionp.textContent = description
     descriptionp.classList.add('description')
@@ -71,30 +103,103 @@ function createNewsContent(urlToImage, content, description) {
     news_card.appendChild(contentp)
 }
 
+function storyCreationPage() {
+    const createStoryBox = document.createElement('fieldset')
+    const header = document.createElement('h1')
+    header.textContent = 'News-Story creation'
+    header.classList.add('headerCreateStory')
+    createStoryBox.appendChild(header)
+    page_content.classList.remove('row')
+    page_content.classList.add('column')
+    const form = document.createElement('form')
+    form.method = 'POST';
+    createStoryBox.appendChild(form)
+    page_content.appendChild(createStoryBox)
+    // page_content.appendChild(form)
+    const title = createInput('title', 'text', 'input title')
+    form.appendChild(title.label)
+    form.appendChild(title.input)
+    const author = createInput('author', 'text', 'input author')
+    form.appendChild(author.label)
+    form.appendChild(author.input)
+    const content = createInput('content', 'text', 'input content')
+    form.appendChild(content.label)
+    form.appendChild(content.input)
+    const image = createInput('image', 'file')
+    form.appendChild(image.label)
+    form.appendChild(image.input)
+    const button = document.createElement('div')
+    button.textContent = 'submit'
+    button.classList.add('button')
+    form.appendChild(button)
+    button.addEventListener('click', (e) => {
+        const {titleValue, authorValue, imageValue, contentValue} = localStorage
+        console.log([titleValue, authorValue, imageValue, contentValue]);
+        
+    })
+}
+function getGuess(e) {
+    const { value } = e.target
+    return Number(value)
+}
+function isCorrectGuess(number, guess) {
+    console.log(guess === number);
+    return guess === number
+}
+
+
+function createInput(name, inputType, placeholder = null) {
+    const label = document.createElement('label')
+    label.textContent = `${name}:`
+    label.for
+    label.classList.add(`${name}Label`, 'label')
+    const input = document.createElement('input')
+    input.type = inputType
+    input.name = name
+    input.placeholder = placeholder
+    input.classList.add(`${name}Input`, 'input')
+    input.addEventListener('change', (e) => {
+        const { value } = e.target
+        localStorage.setItem(`${name}Value`, value)
+    })
+    return { label, input }
+}
+
+
+function contentLoading() {
+    const news = checkLocalStorage()
+    for (let i = 0; i < news.length; i++) {
+        createHomeContent(news[i], i + 1)
+        news[i].id = i + 1
+    }
+    if (!localStorage.data) localStorage.setItem('data', JSON.stringify(news))
+    const news_cards = document.querySelectorAll('.news')
+    for (let i = 0; i < news_cards.length; i++) {
+        news_cards[i].addEventListener('click', e => {
+            e.preventDefault()
+            page_content.innerHTML = ''
+            createNewsContent(news[i].urlToImage, news[i].content, news[i].description)
+        })
+    }
+}
+
+
+
+
+
 
 
 createNavHeader()
-for (let i = 0; i < news.length; i++) {
-    createHomeContent(news[i], i + 1)
-    news[i].id = i + 1
-    // console.log(id);
+contentLoading()
 
-}
-localStorage.setItem('data', JSON.stringify(news))
+// const news_cards = document.querySelectorAll('.news')
+// for (let i = 0; i < news_cards.length; i++) {
+//     let d = []
+//     news_cards[i].addEventListener('click', e => {
+//         e.preventDefault()
+//         if (localStorage.data) d = JSON.parse(localStorage.getItem('data'))
+//         else d = news
+//         createNewsContent(d[i].urlToImage, d[i].content, d[i].description)
+//     })
+// }
 
-const news_cards = document.querySelectorAll('.news')
-
-for (let i = 0; i < news_cards.length; i++) {
-    news_cards[i].addEventListener('click', e => {
-        e.preventDefault()
-        if (localStorage.data) {
-            const d = JSON.parse(localStorage.getItem('data'))
-            console.log(d);
-            console.log(e.target);
-            console.log(d[i].id);
-            console.log(e.target.id);
-            const n = d.find((v, j) => d[j].id == e.target.id)
-            createNewsContent(n.urlToImage, n.content, n.description)
-        }
-    })
-}
--- main.js
+++ main.js
@@ -2,17 +2,17 @@
 // config()
 
 // const api_key = process.env.API_KEY
-const url = `https://newsapi.org/v2/everything?q=tesla&from=2026-01-08&sortBy=publishedAt&apiKey=f408d5751db9483797d84b2826f9c967`
+const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f408d5751db9483797d84b2826f9c967`
 
 export async function getData(link) {
     const res = await fetch(link)
     const result = await res.json()
-        const news = []
+    const news = []
     try {
-        for (let i = 0; i < 10; i++) {           
-            const {author, title, urlToImage, description, content} = result.articles[i]
-            news.push({author, title, urlToImage, description, content})
-        }        
+        for (let i = 0; i < 50; i++) {
+            const { author, title, urlToImage, description, content } = result.articles[i]
+            news.push({ author, title, urlToImage, description, content })
+        }
         return news
     } catch (error) {
         return error
@@ -24,18 +24,25 @@ export async function getData(link) {
 //     for (let i = 0; i < 10; i++) {
 //         const data = await getData(url)[i]
 //         console.log(data);
-        
+
 //         if (data)
 //             news.push(data)
 //     }
 //     return news
 // }
-const news = await getData(url)
 
+const news_data = await getData(url)
+
+function checkLocalStorage() {
+    if (localStorage.data) return JSON.parse(localStorage.getItem('data'))
+    else return news_data
+}
+
+const news = checkLocalStorage()
 
 export {
     news
 }
-// console.log(data);
+console.log(news);
 
 
